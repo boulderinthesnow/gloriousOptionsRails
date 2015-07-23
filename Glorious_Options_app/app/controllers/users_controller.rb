5a69529b403aa49d1343cf361551b13263b3c670
Matthew Williams <mwilliamsnov6@gmail.com>
2015-07-22 22:11:07 -0700

    initial crud working

--- Glorious_Options_app/app/controllers/users_controller.rb
+++ Glorious_Options_app/app/controllers/users_controller.rb
@@ -1,13 +1,54 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.all
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.create(user_params)
+  	if @user.save
+  		redirect_to users_path
+
+  	else
+  		render :new
+  	end
+
   end
 
   def show
   end
 
   def edit
+  	@user = User.find_by(id: params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	if @user.save
+  		redirect_to users_path
+  	else
+  		render :edit
+  	end
+
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to users_path
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(
+  	  		:username,
+  	  		:email,
+  	  		:password
+  	  		)
   end
 end
